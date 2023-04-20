1ab0f2acd04cd089ec6a63b34f72326f2ef09e03
Andrew Wyatt <andrew@fuduntu.org>
2023-04-20 17:18:34 -0400

    Update to use AlmaLinux 8.

--- docker/build.sh
+++ docker/build.sh
@@ -20,4 +20,4 @@ docker image build -t photonbbs .
 ### Describe how to start the container.
 ###
 
-echo -e "\n\nTo use this container, run:\n\ndocker container run -dti --net host --device=/dev/tty0 -v appdata:/appdata:rw -v /dev:/dev -v /lib/modules:/lib/modules --privileged -p 23:23 photonbbs"
+echo -e "\n\nTo use this container, run:\n\ndocker container run -dti --net host --device=/dev/tty0 -v appdata:/appdata:rw -v /dev:/dev -v /lib/modules:/lib/modules --privileged photonbbs"
