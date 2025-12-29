0a9487a9c542641e794395ae4aceee97c2e10562
fewtarius <fewtarius@steamfork.org>
2025-12-29 17:11:35 -0500

    Pull back from the terminal tavern branch

--- doorexec/darkness.sh
+++ doorexec/darkness.sh
@@ -2,5 +2,9 @@
 DOORBAT="darkness.bat"
 export HOME="/opt/photonbbs"
 export TERM="ansi"
+
+# DOSEMU CPU optimization - be nice to other processes
+export DOSEMU_HOGTHRESHOLD=1
+
 dosemu -t -quiet -I "serial { com 1 virtual }" "c:\\doors\\$DOORBAT $2" 2>/dev/null
 reset
